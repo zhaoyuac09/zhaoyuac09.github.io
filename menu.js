ae67a584baf2faaf19f2e17ccacd5709b49cf452
zhaoyuac09 <zo.zhao@siemens-healthineers.com>
2022-06-06 11:54:50 -0400

    new version 2ExperienceExperience022

--- menu.js
+++ menu.js
@@ -2,7 +2,11 @@ document.write('<nav id="menu">\
 						<h2>Menu</h2>\
 						<ul>\
 							<li><a href="index.html">Home</a></li>\
+<<<<<<< HEAD
 							<li><a href="Experience.html">Home</a></li>\
+=======
+							<li><a href="Experience.html">Experience</a></li>\
+>>>>>>> 2b32de8 (new version 2ExperienceExperience022)
 							<li><a href="Education.html">Educations</a></li>\
 							<li><a href="Research.html">Researches</a></li>\
 							<li><a href="Publication.html">Publications</a></li>\
